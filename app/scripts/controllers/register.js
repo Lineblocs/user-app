5338ca00f89117defee42c5608f27b65ad292722
Nadir Hamid <matrix.nad@gmail.com>
2019-09-09 20:21:32 -0600

    add register verify

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -8,10 +8,16 @@
  * Controller of MaterialApp
  */
 angular.module('MaterialApp')
-  .controller('RegisterCtrl', function($scope, $location, $timeout, $q, Backend, SharedPref, $state) {
+  .controller('RegisterCtrl', function($scope, $location, $timeout, $q, Backend, SharedPref, $state, $mdToast) {
 	  $scope.triedSubmit = false;
 	  $scope.passwordsDontMatch = false;
 	  $scope.shouldSplash = false;
+	  $scope.didVerifyCall = false;
+	  $scope.step = 1;
+	  $scope.userId = null;
+	  $scope.token = null;
+	  $scope.invalidCode =false; 
+	  $scope.invalidNumber =false; 
 	$scope.user = {
 		first_name: "",
 		last_name: "",
@@ -19,7 +25,15 @@ angular.module('MaterialApp')
 		password: "",
 		password2: ""
 	};
+	$scope.verify1 = {
+		mobile_number: ""
+	};
+	$scope.verify2 = {
+		confirmation_code: ""
+	};
+
     $scope.submit = function($event, registerForm) {
+		console.log("called submit");
 		$scope.triedSubmit = true;
 		if ($scope.user.password !== $scope.user.password2) {
 			$scope.passwordsDontMatch = true;
@@ -29,18 +43,85 @@ angular.module('MaterialApp')
 		}
 		if (registerForm.$valid) {
 			var data = angular.copy( $scope.user );
-			$scope.shouldSplash = true;
 			Backend.post("/register", data).then(function( res ) {
-				var token = res.data;
-				SharedPref.setAuthToken( token );
-		        $state.go('dashboard-user-welcome', {});
+				$scope.token = res.data.token;
+				$scope.userId = res.data.userId;
+				$scope.step = 2;
 			});
 			return;
 		}
       	return false;
 
-    }
+	}
 
+	$scope.submitVerify1Form = function($event, verify1Form) {
+		console.log("called submitVerify1Form");
+		$scope.triedSubmit = true;
+		if (verify1Form.$valid) {
+			var data = angular.copy( $scope.verify1 );
+			data.userId = $scope.userId;
+			Backend.post("/registerSendVerify", data).then(function( res ) {
+				var data = res.data;
+				if (res.data.valid) {
+					$scope.didVerifyCall = true;
+					$scope.invalidNumber = false;
+					return;
+				}
+				$scope.invalidNumber = true;
+				//$scope.showNumberInvalid = true;
+			});
+			return;
+		}
+		return false;
+	}
+
+	$scope.submitVerify2Form = function($event, verify2Form) {
+		console.log("called submitVerify2Form");
+		$scope.triedSubmit = true;
+		if (verify2Form.$valid) {
+			var data = angular.copy( $scope.verify2 );
+			data.userId = $scope.userId;
+			Backend.post("/registerVerify", data).then(function( res ) {
+				var isValid = res.data.isValid;
+				if (isValid) {
+					$scope.shouldSplash = true;
+					SharedPref.setAuthToken( $scope.token );
+					var data = { "userId": $scope.userId };
+					$scope.invalidCode = false;
+					Backend.post("/userSpinup", data).then(function( res ) {
+						var data = res.data;
+						if ( data.success ) {
+							$state.go('dashboard-user-welcome', {});
+							return;
+						}
+						$mdToast.show(
+						$mdToast.simple()
+							.textContent('Error occured while creating your account. please account support')
+							.position("top right")
+							.hideDelay(1000*10)
+						);
+					});
+				} else {
+					$scope.invalidCode = true;
+				}
+			});
+			return;
+		}
+		return false;
+	}
+
+	$scope.recall = function() {
+		var data = angular.copy( $scope.verify1 );
+		data.userId = $scope.userId;
+		Backend.post("/registerSendVerify", data).then(function( res ) {
+           $mdToast.show(
+          $mdToast.simple()
+            .textContent('You will be called shortly.')
+            .position("top right")
+            .hideDelay(3000)
+		);
+		   });
+	}
     $scope.authenticate = function() {
 
     	var defer = $q.defer();
