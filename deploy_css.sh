eec12979af33340005d9f12fe2b882611ba674fa
Nadir Hamid <matrix.nad@gmail.com>
2019-09-14 00:58:09 +0000

    add CSS deploy script

---
+++ deploy_css.sh
@@ -0,0 +1,5 @@
+rm -rf app/styles/app-blue.css
+rm -rf app/styles/styles-dir
+node-sass app/styles/app-blue.scss -o app/styles/styles-dir
+mv  app/styles/styles-dir/app-blue.css app/styles/app-blue.css
+rm -rf app/styles/styles-dir
