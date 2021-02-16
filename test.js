91ed7f7818dfceaf8e71833663a91e0f65ce77b0
proseigner <matrix.nad@gmail.com>
2021-02-15 17:17:58 -0700

    Cleanup project

--- test.js
+++
@@ -1,7 +0,0 @@
-
-var mergeTemplates = require('./merge_templates.js');
-var fs = require("fs");
-mergeTemplates().then(function(output) {
-        fs.writeFileSync("./app/templates.html", output);
-    });
-
