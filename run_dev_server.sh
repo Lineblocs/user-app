cac5a13a53e7613e9bec5987f00905a858486d8f
Nadir Hamid <matrix.nad@gmail.com>
2023-01-12 02:11:21 +0000

    fix deployment issues

--- run_dev_server.sh
+++ run_dev_server.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
-#nvm install v11.10.1
-#nvm use v11.10.1
-#npm -g install gulp
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install v11.10.1
+nvm use v11.10.1
+npm -g install gulp
+npm -g install bower
+npm install
+bower install --allow-root
 gulp compress-css
 gulp scripts
 gulp compress-js
