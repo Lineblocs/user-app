364e8d20fc16d146d9e27e987c8ebba6d1b544e9
Chandrashekhar Kumar <chsinghshekhar@gmail.com>
2023-06-15 12:46:40 +0530

    Finished update the cancel subscription on UA-77

--- app/scripts/controllers/CancelSubscription.js
+++ app/scripts/controllers/CancelSubscription.js
@@ -9,9 +9,10 @@
  */
 angular.module('Lineblocs').controller('CancelSubscriptionCtrl', function ($scope, $location, $timeout, $q, Backend, $shared, $state, $mdToast, $mdDialog, $window) {
   $shared.updateTitle("Cancel Subscription");
+  $scope.cancelSubscription = false;
   $shared.endIsCreateLoading();
   $scope.cancelSubscription = function ($event) {
-
+    $scope.cancelSubscription = true;
     const confirm = $mdDialog.confirm()
       .title('Are you sure you want to cancel your subscription?')
       .textContent('You will not be able to use Lineblocs until you subscribe again.')
@@ -23,9 +24,12 @@ angular.module('Lineblocs').controller('CancelSubscriptionCtrl', function ($scop
       $shared.isCreateLoading = true;
       Backend.post("/billing/discontinue").then(function (res) {
         $mdToast.show($mdToast.simple().textContent('Subscription cancelled').position("top right").hideDelay(3000));
-        $state.go('billing');
+        $scope.cancelSubscription = false;
         $shared.endIsCreateLoading();
       });
     });
+  }
+  $scope.goBilling = function() {
+    $state.go('billing');
   }
 });
