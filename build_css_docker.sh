b88a6a32a5c705e13b824de56a98a543fe029495
Nadir Hamid <matrix.nad@gmail.com>
2023-06-27 17:32:08 -0600

    WIP: improve docker build files

---
+++ build_css_docker.sh
@@ -0,0 +1,2 @@
+gulp compress-css
+node-sass app/styles/app-blue.scss -o app/styles/styles
--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
-./deploy_css_docker.sh
-./deploy_js_docker.sh
+./build_css_docker.sh
+./build_js_docker.sh
 cp ./app/index-prod.html ./app/index.html
 cp ./dist/main.min.js ./app/scripts/
