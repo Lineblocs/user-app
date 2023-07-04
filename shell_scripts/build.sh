3140e1edd998de5898c9135335bd3d599e74b1b2
Nadir Hamid <matrix.nad@gmail.com>
2023-07-04 12:35:17 -0600

    move scripts to shell_scripts folder

--- shell_scripts/build.sh
+++ shell_scripts/build.sh
@@ -1,4 +1,4 @@
-./build_css.sh
-./build_js.sh
+./shell_scripts/build_css.sh
+./shell_scripts/build_js.sh
 cp ./app/index-prod.html ./app/index.html
 PATH=$PATH:/usr/share/node-v11.10.1-linux-x64/bin/ gulp compress-css
