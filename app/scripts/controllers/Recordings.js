f448a2de97a8f43aa2078e86e53f00c591bc0a84
Hardik Prajapati <prajapatihardik1212@gmail.com>
2026-05-11 22:54:46 +0530

    UA-136: add option for user to download single recording or all recordings

--- app/scripts/controllers/Recordings.js
+++ app/scripts/controllers/Recordings.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of Lineblocs
  */
-angular.module('Lineblocs').controller('RecordingsCtrl', function ($scope, Backend, pagination, $location, $state, $mdDialog, $sce, $shared, $q, $mdToast, $stateParams) {
+angular.module('Lineblocs').controller('RecordingsCtrl', function ($scope, Backend, pagination, $location, $state, $mdDialog, $sce, $shared, $q, $mdToast, $stateParams, $http) {
 	  $shared.updateTitle("Recordings");
   $scope.settings = {
     page: 0
@@ -92,6 +92,80 @@ angular.module('Lineblocs').controller('RecordingsCtrl', function ($scope, Backe
     }, function() {
     });
   }
+
+  function parseFilenameFromDisposition(contentDisposition) {
+    if (!contentDisposition) {
+      return null;
+    }
+    var encodedMatch = /filename\*=UTF-8''([^;]+)/i.exec(contentDisposition);
+    if (encodedMatch && encodedMatch[1]) {
+      return decodeURIComponent(encodedMatch[1]);
+    }
+    var plainMatch = /filename="?([^";]+)"?/i.exec(contentDisposition);
+    return plainMatch && plainMatch[1] ? plainMatch[1] : null;
+  }
+
+  function triggerDownloadFromResponse(res, defaultFileName) {
+    var contentType = res.headers('Content-Type') || 'application/octet-stream';
+    var contentDisposition = res.headers('Content-Disposition') || '';
+    var fileName = parseFilenameFromDisposition(contentDisposition) || defaultFileName;
+    var blob = new Blob([res.data], { type: contentType });
+    var url = window.URL.createObjectURL(blob);
+    var link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(url);
+  }
+
+  $scope.downloadRecording = function(recording) {
+    if (!recording || !recording.id) {
+      return;
+    }
+    $shared.isLoading = true;
+    $http.post(
+      createUrl('/recording/downloadRecordings'),
+      { recording_ids: recording.id },
+      { responseType: 'blob' }
+    ).then(function(res) {
+      triggerDownloadFromResponse(res, 'recording-' + recording.id + '.wav');
+    }).catch(function() {
+      $shared.showError('Unable to download recording. Please try again.');
+    }).finally(function() {
+      $shared.endIsLoading();
+    });
+  };
+
+  $scope.downloadAllRecordings = function() {
+    var checked = ($scope.recordings || []).filter(function(recording) {
+      return recording && recording.checked;
+    });
+    var ids = checked.map(function(recording) {
+      return recording && recording.id;
+    }).filter(function(id) {
+      return !!id;
+    });
+
+    if (!ids.length) {
+      $shared.showError('Please select one or more recordings to download.');
+      return;
+    }
+
+    $shared.isLoading = true;
+    $http.post(
+      createUrl('/recording/downloadRecordings'),
+      { recording_ids: ids.join(',') },
+      { responseType: 'blob' }
+    ).then(function(res) {
+      triggerDownloadFromResponse(res, 'recordings.zip');
+    }).catch(function() {
+      $shared.showError('Unable to download recordings. Please try again.');
+    }).finally(function() {
+      $shared.endIsLoading();
+    });
+  };
 
   $scope.load();
 });
